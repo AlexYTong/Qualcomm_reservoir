1a148d941285bef9c1a1e308622a6077fdce78d3
AlexYTong <pi@raspberrypi.(none)>
2015-02-14 07:46:17 -0500

    changed to stable git release

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@
 #Created on February 14, 2015 at ID hack for setup of a webcaching raspberry pi
 #setup .sh attempts to install polipo through the package manager apt-get.
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install polipo
-sudo cp config /etc/polipo
+apt-get install texinfo -Y
+tar -zxvf polipo.tar.gz
+cd polipo
+make all
+sudo make install
+echo "polipo" >> ~/.bashrc
 
 ###############################################################################
 End of File
