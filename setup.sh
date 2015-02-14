89365f63fee9367f14bbe58e8c49bda654e700a6
AlexYTong <pi@raspberrypi.(none)>
2015-02-14 08:09:38 -0500

    readme changes

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #setup.sh
-
+# WARNING: This script MUST be used with the sudo command
+# usage: sudo sh setup.sh
+  
 #Created on February 14, 2015 at ID hack for setup of a webcaching raspberry pi
 #setup .sh attempts to install polipo through the package manager apt-get.
+#
+# 
+#
+#
 
 apt-get install texinfo -Y
 tar -zxvf polipo.tar.gz
 cd polipo
 make all
-sudo make install
+make install
 echo "polipo" >> ~/.bashrc
+cp -f inittab /etc/inittab
 
 ###############################################################################
 End of File
